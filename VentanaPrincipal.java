c9599ce52a6ce4d8ba7cb500d28d378e24bb9c76
VladimirAh <vladisazp@gmail.com>
2019-10-17 00:23:01 -0500

    GUI v2.0

--- VentanaPrincipal.java
+++ VentanaPrincipal.java
@@ -7,8 +7,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.Color;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
 import javax.swing.JTabbedPane;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JTable;
@@ -19,82 +17,112 @@ import javax.swing.JScrollPane;
 import javax.swing.JOptionPane;
 
 public class VentanaPrincipal extends JFrame implements ActionListener {
-	private JPanel jpNorth, jpSouth, jpCenter, jpPendientes, jpHorario, jpControl;
-	//private JMenuBar jMB;
-	//private JMenu jM1, jM2, jM3;
-	private JTabbedPane JTvista;
-	private DefaultTableModel modelPendientes;
-	private JTable jtbPendientes;
-	private GridBagConstraints constraints;
-	private JLabel lblInfoSouth;
-	private JScrollPane scrollPane;
-	public static void main(String[] args) {
-		VentanaPrincipal vp = new VentanaPrincipal();
-		vp.setVisible(true);
-	}
+	private JPanel jpNorth; //Panel superior donde estaran los parametros del metodo
+    private JScrollPane jpCenter; //Panel central donde el usuario colocara las restricciones
+    private JPanel jpSouth; // Panel inferior que contendra el boton de Resolver
+
+    private JButton btnResolver; //Boton que indicara al programa que comienze el metodo
+
+    private JLabel lblNumVar; private JTextField txtNumVar;
+    private JLabel lblNumRes; private JTextField txtNumRes;
+    private JLabel lblFuncionObj; private JScrollPane jpFuncionObj;
+    private JLabel lblMaxOMin; 
+    private JLabel lblIngresaRes;
+
+
+
+    private GridBagConstraints constraints; //Para el GridBagLayout
+
     public VentanaPrincipal() {
         super();
         configurarVentana();
         inicializarComponentes();
     }
 
+    //Configuracion general de la ventana principal
     private void configurarVentana() {
-        this.setTitle("Solver Simplex");
-        this.setSize(1000, 600);
-        this.setLocationRelativeTo(null);
-        //$this.setLayout(null);
-        //$this.setResizable(false);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Solver Simplex"); //Titulo de la ventana 
+        this.setSize(1000, 600); // Tamaño de la ventana
+        this.setLocationRelativeTo(null); //Localiza la ventana en el centro de la pantalla
+        //this.setResizable(false); //Evitar que se pueda cambiar el tamaño de la ventana
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Permite que se cierre la ventana
+        //El JFrame (VentanaPrincipal) usara el layout BorderLayout
     }
 
+    //Inicializar los componentes que estaran dentro de VentanaPrincipal
     private void inicializarComponentes() {
-    	//jMB = new JMenuBar();
-    	jpNorth = new JPanel();
-    	jpCenter = new JPanel();
+    	jpNorth = new JPanel(); 
+    	jpCenter = new JScrollPane();
     	jpSouth = new JPanel();
-    	// jM1 = new JMenu("Menu1");
-    	// jM2 = new JMenu("Menu2");
-    	// jM3 = new JMenu("Menu3");
-    	JTvista = new JTabbedPane();
-    	jpPendientes = new JPanel();
-    	jpHorario = new JPanel();
-    	jpControl = new JPanel();
-    	constraints = new GridBagConstraints();
-    	lblInfoSouth = new JLabel( "" + new java.util.Date() );
 
-    	jtbPendientes = new JTable(modelPendientes);
-    	jtbPendientes.setFillsViewportHeight(true);
-    	scrollPane = new JScrollPane(jtbPendientes);    	
-    	//setJMenuBar(jMB);
-    	jpNorth.setBackground(Color.GRAY);
-    	//jMB.add(jM1); jMB.add(jM2); jMB.add(jM3);
-    	add(jpNorth, BorderLayout.NORTH);
-    	add(jpCenter, BorderLayout.CENTER);
-    	add(jpSouth, BorderLayout.SOUTH);
-    	JTvista.addTab("Pendientes", scrollPane);
-    	JTvista.addTab("Horario", jpHorario);
-    	jpCenter.setLayout(new GridBagLayout());
-    	constraints.weighty = 1.0;
-    	constraints.gridx = 0;
-		constraints.gridy = 0;
-		constraints.gridwidth = 3;
-		constraints.weightx = 5.0;
-		constraints.fill = GridBagConstraints.BOTH;
-		jpCenter.add(JTvista, constraints);
-		constraints.weightx = 1.0;
-		constraints.gridx = 5;
-		constraints.gridy = 0;
-		constraints.fill = GridBagConstraints.BOTH;
-		jpCenter.add(jpControl, constraints);
-		jpControl.setLayout(new GridBagLayout());
-		constraints.weightx = 0;
-		constraints.weighty = 0;
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		jpSouth.add(lblInfoSouth);
+    	add(jpNorth, BorderLayout.NORTH); //Colocamos jpNorth en la parte superior
+    	add(jpCenter, BorderLayout.CENTER); //Colocamos jpCenter en la parte central
+    	add(jpSouth, BorderLayout.SOUTH); //Colocamos jpSouth en la parte inferior
+
+        constraints = new GridBagConstraints();
+        jpNorth.setLayout(new GridBagLayout()); //Para el panel superior usaremos el GridBagLayout
+        
+        //Ubicamos la etiqueta "Numero de variables:" en el layout
+        constraints.weighty = 1.0;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 10;
+        constraints.weightx = 4.0;
+        constraints.fill = GridBagConstraints.BOTH;
+        lblNumVar = new JLabel("Numero de variables:");
+        jpNorth.add(lblNumVar, constraints);
+
+        //Ubicamos el campo de texto para ingresa el numero de variables en el layout
+        constraints.weightx = 1.0;
+        constraints.gridx = 4;
+        constraints.gridy = 0;
+        constraints.fill = GridBagConstraints.BOTH;
+        txtNumVar = new JTextField();
+        jpNorth.add(txtNumVar, constraints);
+
+        //Ubicamos la etiqueta "Selecciona una opcion" en el layout
+        constraints.weightx = 5.0;
+        constraints.gridx = 5;
+        constraints.gridy = 0;
+        constraints.fill = GridBagConstraints.BOTH;
+        lblMaxOMin = new JLabel("Selecciona una opcion");
+        jpNorth.add(lblMaxOMin, constraints);
+
+        //Ubicamos la etiqueta "Numero de restricciones:" en el layout
+        constraints.weightx = 1.0;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        lblNumRes = new JLabel("Numero de restricciones:");
+        jpNorth.add(lblNumRes, constraints);
+
+        //Ubicamos el campo de texto para ingresa el numero de restricciones en el layout
+        constraints.weightx = 1.0;
+        constraints.gridx = 3;
+        constraints.gridy = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        txtNumRes = new JTextField();
+        jpNorth.add(txtNumRes, constraints);
+        
+        
+
+        lblIngresaRes = new JLabel("Ingresa las restricciones:");
+
+
+
+        btnResolver = new JButton("Resolver");
+        jpSouth.add(btnResolver);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
+    }
+
+    //Funcion principal
+    public static void main(String[] args) {
+        VentanaPrincipal vp = new VentanaPrincipal(); //Se crea la ventana principal
+        vp.setVisible(true); // Se hace visible la ventana
     }
 }
